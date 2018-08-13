542527d651fb0a2fa89af0ab48558c27faa13300
JATIN KUMAR <jatinnith@gmail.com>
2018-08-13 17:42:56 +0000

    Create Moore's Voting Algorithm.cpp

---
+++ More Algorithms/Moore's Voting Algorithm.cpp
@@ -0,0 +1,54 @@
+// Implementer: Jatin Kumar
+// Problem: Given an array of size n.
+// find the element which occure more than n times
+// Moore's Voting Algorithm is used
+// Time Complexity: O(n)
+
+#include<iostream>
+using namespace std;
+int main()
+{
+	int test;
+	cin>>test;
+	while(test--)
+	{
+		int n;
+		cin>>n;
+		int array[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>array[i];
+		}
+		int count=0,candidate=array[0];
+		for(int i=0;i<n;i++)
+		{
+			if(array[i]==candidate)
+			{
+				count++;
+			}
+			else
+			{
+				count--;
+			}
+			if(count==0)
+			{
+				candidate=array[i];
+				count=1;
+			}
+		}
+		count=0;
+		for(int i=0;i<n;i++)
+		{
+			if(array[i]==candidate)
+				count++;
+		}
+		if(count>n/2)
+		{
+			cout<<candidate<<endl;
+		}
+		else
+		{
+			cout<<"NO Majority Element"<<endl;
+		}
+	}
+}
